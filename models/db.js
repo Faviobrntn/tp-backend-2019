ca5070579b42fdd814a8c9cb5002c249e2c67d00
Favio <faviobarnatan@gmail.com>
2019-10-10 21:20:36 -0300

    correccion de rutas

--- models/db.js
+++ models/db.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
 
-const URI = 'mongodb://localhost/nombre_db';
+const URI = 'mongodb://localhost/TP-Backend';
 
-mongoose.connect(URI)
+mongoose.connect(URI,
+    {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
     .then(db => console.log("Conectado a la DB"))
     .catch(err => console.error(err));
 
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,7 @@
 var router = require('express').Router();
 
 router.use('/api/usuarios', require('./usuarios.routes'));
-app.use('/api/habitaciones', require('./habitaciones.routes'))
-app.use('/api/reservas', require('./reservas.routes'))
+router.use('/api/habitaciones', require('./habitaciones.routes'))
+router.use('/api/reservas', require('./reservas.routes'))
 
 module.exports = router;
